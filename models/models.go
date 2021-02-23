f2645ad24bc2bf09e110c04e654c93f794e785e0
ChandrikaHarish <chandrika.bharish@gmail.com>
2021-02-23 13:25:52 +0530

    Create models.go

---
+++ models/models.go
@@ -0,0 +1,10 @@
+package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+//Create model
+type User struct {
+	ID    primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name  string             `json:"name,omitempty" bson:"name,omitempty"`
+	Place string             `json:"place,omitempty" bson:"place,omitempty"`
+}
