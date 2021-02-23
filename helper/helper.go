29a55d38107c11835c184d9439f81962cd07628f
ChandrikaHarish <chandrika.bharish@gmail.com>
2021-02-23 13:41:11 +0530

    Update helper.go

--- helper/helper.go
+++ helper/helper.go
@@ -8,7 +8,6 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -54,26 +53,4 @@ func GetError(err error, w http.ResponseWriter) {
 
 	w.WriteHeader(response.StatusCode)
 	w.Write(message)
-}
-
-// Configuration model
-type Configuration struct {
-	Port             string
-	ConnectionString string
-}
-
-// GetConfiguration method basically populate configuration information from .env and return Configuration model
-func GetConfiguration() Configuration {
-	err := godotenv.Load("./.env")
-
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
-
-	configuration := Configuration{
-		os.Getenv("PORT"),
-		os.Getenv("CONNECTION_STRING"),
-	}
-
-	return configuration
 }
